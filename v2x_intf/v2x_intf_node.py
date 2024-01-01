3f7394e6f9f2a06e367bdcba26b60d9db51243a7
Woojin Jeong <jeong.woojin@gmail.com>
2024-01-01 23:05:16 +0900

    Use v2x_intf_node name
    Use v2x_intf_node name

--- v2x_intf/v2x_intf_node.py
+++ v2x_intf/v2x_intf_node.py
@@ -1,5 +1,29 @@
+import rclpy as rclpy
+from rclpy.node import Node
+
+class RecognitionSubscriber(Node):
+    def __init__(self):
+        super().__init__('recognition_subscriber')
+        self.subscription = self.create_subscription(
+            # Recognition,
+            'caselab/recognition',
+            self.recognition_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def recognition_callback(self, msg):
+        print('I heard: "%s"' % msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    recognition_subscriber = RecognitionSubscriber()
+    rclpy.spin(recognition_subscriber)
+
+    recognition_subscriber.destroy_node()
+    rclpy.shutdown()
+
 def main():
-    print('Hi from v2x_intf.')
+    print('Hi from v2x_subscriber.')
 
 
 if __name__ == '__main__':
