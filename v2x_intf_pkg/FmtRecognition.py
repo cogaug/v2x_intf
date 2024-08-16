f0934dbbabc04ce40020d7e976eb085fb2a5e9de
ujeani <jeong.woojin@gmail.com>
2024-08-16 11:54:33 +0900

    Update FmtRecognition.py
    miss comma

--- v2x_intf_pkg/FmtRecognition.py
+++ v2x_intf_pkg/FmtRecognition.py
@@ -82,7 +82,7 @@ class recognition_data_fixed_part_type(ctypes.Structure):
 class v2x_recognition_msg_type(ctypes.Structure):
     _fields_ = [
         ("hdr", v2x_intf_hdr_type),     # v2x_intf_hdr_type structure
-        ("data", recognition_data_fixed_part_type) # recognition_data_fixed_part_type structure
+        ("data", recognition_data_fixed_part_type), # recognition_data_fixed_part_type structure
         ("objects", ctypes.POINTER(DetectedObjectCommonData)) # Pointer to DetectedObjectCommonData array
     ]
 
